ca7733298f31416f5dc558e4a2b7a5f7b24ddae9
OUSSAMA OULOUARK <alioulouark@gmail.com>
2023-03-23 16:25:13 +0100

    creat project and add photos

---
+++ projectpassman/app/src/main/java/com/example/projectpassman/Mainactivity.java
@@ -0,0 +1,14 @@
+package com.example.projectpassman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mainactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainactivity);
+    }
+}
